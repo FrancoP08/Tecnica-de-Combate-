dc13a69a855534151953df5ed094e6b60750bfb8
Franco Pasqualino <fpasqualino@frba.utn.edu.ar>
2021-04-29 00:21:28 -0300

    Actualizacion-Codigo Finalizado

--- src/Library.hs
+++ src/Library.hs
@@ -1,5 +1,47 @@
 module Library where
 import PdePreludat
+type Poder = Number  
 
-doble :: Number -> Number
-doble numero = numero + numero
+double :: Number -> Number
+double = (*2)
+
+type Horas = Number
+
+poderDelGolpe :: Horas -> Number
+poderDelGolpe = (*15) 
+
+type Objetivo = String
+
+fortalezaDelObjetivo :: Objetivo -> Number
+fortalezaDelObjetivo = (*2).length
+
+presionDelGolpe :: Horas -> Objetivo -> Number
+presionDelGolpe horasDeEntrenamiento nombreDelObjetivo = poderDelGolpe horasDeEntrenamiento / fortalezaDelObjetivo nombreDelObjetivo
+
+
+
+type Golpe = Objetivo -> Number
+
+tecnicaGomuGomu :: Golpe 
+tecnicaGomuGomu = presionDelGolpe 180 
+
+tecnicaGolpesConsecutivos :: Golpe
+tecnicaGolpesConsecutivos = presionDelGolpe 240
+
+
+
+objetivoEsDificil :: Objetivo -> Bool
+objetivoEsDificil nombreDelObjetivo = (tecnicaGomuGomu nombreDelObjetivo) < 100 
+
+objetivoEsAccesible :: Objetivo -> Bool
+objetivoEsAccesible nombreDelObjetivo = medicionDelObjetivo nombreDelObjetivo >= 200 && medicionDelObjetivo nombreDelObjetivo <= 400
+
+medicionDelObjetivo :: Golpe
+medicionDelObjetivo = tecnicaGolpesConsecutivos.objetivoFocalizado
+
+type Focalizar = Objetivo -> String
+
+objetivoFocalizado :: Focalizar
+objetivoFocalizado = take 7 
+
+
